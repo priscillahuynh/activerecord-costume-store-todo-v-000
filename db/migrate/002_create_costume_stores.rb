ac42f0ce8b31813c954abe670b84a6b70411d6f7
Priscilla H <priscillamhuynh@gmail.com>
2019-01-04 05:17:45 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@ class CreateCostumeStore < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.string :location
       t.string :image_url
       t.integer :size
     end
