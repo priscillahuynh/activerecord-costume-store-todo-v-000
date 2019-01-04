904ec9dda395920cf8bda2a6e24f247ca086d435
Priscilla H <priscillamhuynh@gmail.com>
2019-01-04 06:10:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,4 @@
-class CreateHauntedHouses < ActiveRecord::Migration
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
