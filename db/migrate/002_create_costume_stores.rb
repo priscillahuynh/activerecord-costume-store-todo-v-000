7b497f36b7dd756a1061c796fab2ffaf8ce7d8a9
Priscilla H <priscillamhuynh@gmail.com>
2019-01-04 05:18:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStore < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :employee_count
     end
   end
